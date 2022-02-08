85b615f018c58eee00c5b26517dab6f852b62d30
BK201-Drama <1243532812@qq.com>
2022-02-09 02:36:12 +0800

    master: 搭建了一些内容

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = function(app){
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://bk201-drama.app.cloudendpoint.cn/api',
+      changeOrigin: true, 
+    })
+  )
+}
